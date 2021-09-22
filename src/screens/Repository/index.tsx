cc4054256c22c558dc46c940654f1af6930a7cf5
Eduardo <bisinella7@gmail.com>
2021-09-22 08:10:30 -0300

    first commit

---
+++ src/screens/Repository/index.tsx
@@ -0,0 +1,111 @@
+import React from 'react';
+import { useRoute } from '@react-navigation/core';
+import { Linking } from 'react-native';
+import { useRepositories } from '../../hooks/useRepositories';
+
+import { Background } from '../../components/Background';
+import { Card } from '../../components/Card';
+
+import {
+  Container,
+  RepoInfo,
+  OwnerAvatar,
+  TextGroup,
+  Description,
+  RepoStats,
+  Stars,
+  StarsCounter,
+  StarsText,
+  Forks,
+  ForksCounter,
+  ForksText,
+  OpenIssues,
+  OpenIssuesCounter,
+  OpenIssuesText,
+  IssuesList,
+} from './styles';
+import { TitleAnimation } from './TitleAnimation';
+
+interface RepositoryParams {
+  repositoryId: number;
+}
+
+export function Repository() {
+  const { params } = useRoute();
+  const { repositoryId } = params as RepositoryParams;
+  const { findRepositoryById } = useRepositories();
+  const repository = findRepositoryById(repositoryId);
+
+  function handleIssueNavigation(issueUrl: string) {
+    // TODO - use Linking to open issueUrl in a browser
+    Linking.openURL(issueUrl);
+  }
+
+  return (
+    <Background>
+      <Container>
+        <RepoInfo>
+          <OwnerAvatar source={{ uri: repository.issues_url }} />
+
+          <TextGroup>
+            <TitleAnimation>{repository.full_name}</TitleAnimation>
+
+            <Description numberOfLines={2}>
+              {
+                //TODO - repository description
+                repository.description
+              }
+            </Description>
+          </TextGroup>
+        </RepoInfo>
+
+        <RepoStats>
+          <Stars>
+            <StarsCounter>
+              {
+                // TODO - repository stargazers count
+              }
+            </StarsCounter>
+            <StarsText>Stars</StarsText>
+          </Stars>
+
+          <Forks>
+            <ForksCounter>
+              {
+                // TODO - repository forks count
+              }
+            </ForksCounter>
+            <ForksText>Forks</ForksText>
+          </Forks>
+
+          <OpenIssues>
+            <OpenIssuesCounter>
+              {
+                // TODO - repository issues count
+                repository.issues.length
+              }
+            </OpenIssuesCounter>
+            <OpenIssuesText>Issues{'\n'}Abertas</OpenIssuesText>
+          </OpenIssues>
+        </RepoStats>
+
+        <IssuesList
+          data={repository.issues}
+          keyExtractor={(issue) => String(issue.id)}
+          showsVerticalScrollIndicator={false}
+          renderItem={({ item: issue }) => (
+            <Card
+              data={{
+                id: issue.id,
+                title: issue.title,
+                subTitle: issue.user.login,
+              }}
+              // TODO - onPress prop calling
+              onPress={() => handleIssueNavigation(issue.html_url)}
+            />
+          )}
+        />
+      </Container>
+    </Background>
+  );
+}
